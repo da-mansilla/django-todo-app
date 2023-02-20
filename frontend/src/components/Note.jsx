d8a66839181cbd449eb7cb41cc4ce23a7818f676
agustin1019 <agustin.mansilla@heritas.com.ar>
2023-02-20 12:50:02 -0300

    change

--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -3,7 +3,7 @@ const Note = () => {
     return (
         <div className='my-5 bg-white p-2 rounded-2xl drop-shadow-xl '>
             <h2 className="text-2xl pt-3  ml-7 text-blue-500 ">
-                Titulo de la Nota
+                Titulo de La Nota
             </h2>
             <div className="flex justify-center py-5">
                 <input className="text-xl rounded-2xl border border-gray-300 w-11/12 h-20" type="textarea" />
